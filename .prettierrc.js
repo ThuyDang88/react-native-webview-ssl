1d0ec4b12e642c1185c347a9fc1a4eff72b55e77
Thuy Dang <dangthithuydlu@gmail.com>
2020-12-16 15:19:47 +0700

    - Add .prettierc.js

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // Enables semicolons at the end of statements
+    semi: true,
+    // Formats strings with single quotes ('') instead of quotes ("")
+    singleQuote: true,
+    // Adds a trailing comma at the end of all lists (including function arguments)
+    trailingComma: 'all',
+  };
